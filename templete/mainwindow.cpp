96fb5cbcbc131f90c4a3ba9421b025f5b15cdbae
kangli <kanglicn@live.com>
2022-04-04 08:43:11 +0800

    自定义窗口

--- templete/mainwindow.cpp
+++ templete/mainwindow.cpp
@@ -1,16 +1,38 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setWindowFlags(Qt::FramelessWindowHint|windowFlags());
     this->setWindowTitle(QApplication::translate("toplevel","Top-level widget"));
+
+    QHBoxLayout* title = new QHBoxLayout();
+    title->setContentsMargins(0, 0, 0, 0);
+    title->setSpacing(0);
+    title->addStretch();
+    title->addWidget(ui->btnMinimized);
+    title->addWidget(ui->btnMaximized);
+    title->addWidget(ui->btnClose);
+
+    QWidget* widgetTitle = new QWidget();
+    widgetTitle->setLayout(title);
+
+    QVBoxLayout* centeral = new QVBoxLayout();
+    centeral->setSpacing(0);
+    centeral->setContentsMargins(0, 0, 0, 0);
+    centeral->addWidget(widgetTitle);
+    centeral->addStretch();
+
+
+    ui->centralwidget->setLayout(centeral);
+
     QObject::connect(ui->btnClose,SIGNAL(clicked()),this,SLOT(close()));
-    QObject::connect(ui->btnMaximized,SIGNAL(toggle()),this,SLOT(showMaximized()));
+    QObject::connect(ui->btnMaximized,SIGNAL(clicked()),this,SLOT(showMaximized()));
     QObject::connect(ui->btnMinimized, SIGNAL(clicked()), this, SLOT(showMinimized()));
     QToolButton* btn=new QToolButton();
 
---
+++ templete/mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMouseEvent>
+#include <QPoint>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void mousePressEvent(QMouseEvent *event);//鼠标点击
+    void mouseMoveEvent(QMouseEvent *event);//鼠标移动
+    void mouseReleaseEvent(QMouseEvent *event);//鼠标释放
+private:
+    Ui::MainWindow *ui;
+    QPoint mPoint;
+};
+#endif // MAINWINDOW_H
